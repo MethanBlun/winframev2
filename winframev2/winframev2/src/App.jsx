461a5e81354191fdb120e8f487e785621cf64344
Osirus Jack <methanblun@gmail.com>
2024-05-10 11:45:11 +0000

    deps

---
+++ winframev2/winframev2/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className=' text-red-700 bg-slate-950'>helloworld</div>
+    </>
+  )
+}
+
+export default App
